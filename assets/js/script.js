94ad480f4cda0742855af95ca45ce51e02af8c21
J Brennan <J_Brennan@me.com>
2021-03-17 21:16:17 -0400

    need to get uv api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 
 var citySearchEl = document.querySelector("#submit");
 var cityName = document.querySelector('#citySearch');
+const appId = "f642abd07680d52d301698bab489005d"
 
 var cityClickHandler = function (event) {
     event.preventDefault()
@@ -11,20 +12,29 @@ var cityClickHandler = function (event) {
     var wind = document.querySelector("#wind");
     var uv = document.querySelector("#uv");
     var city = cityName.value;
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=f642abd07680d52d301698bab489005d";
- 
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + appId;
+
+
     fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      
-      temp.textContent = data.main.temp;
-        // humidity.value = data.main.
-
-
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            temp.textContent = data.main.temp;
+            humidity.value = data.main.humidity;
+            wind.value = data.wind.speed;
+            uvRequest(lat, lon);
+        });
 }
 
+function uvRequest(lat, lon) {
+    var uvRequestUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + appId;
+    fetch(uvRequestUrl)
+        .then(function (Data) {
+            console.log("uvData:"+Data);
+        });
+}
 citySearchEl.addEventListener('click', cityClickHandler);
