533c221fb54e73f28a08bca3b991be6cc9de4938
J Brennan <J_Brennan@me.com>
2021-03-31 18:30:19 -0400

    added uv index need to add local storage for previous searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,44 @@
 
 // fetch request gets a list of all the repos for the node.js organization
 
-var citySearchEl = document.querySelector("#submit");
-var cityName = document.querySelector('#citySearch');
-const appId = "f642abd07680d52d301698bab489005d"
+const citySearchEl = document.querySelector("#submit");
+const cityName = document.querySelector('#citySearch');
+const appId = "f642abd07680d52d301698bab489005d";
 
-var cityClickHandler = function (event) {
+cityClickHandler = (event) => {
     event.preventDefault()
-    var temp = document.querySelector("#temp");
-    var humidity = document.querySelector("#humidity");
-    var wind = document.querySelector("#wind");
-    var uv = document.querySelector("#uv");
-    var city = cityName.value;
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + appId;
+    const tempEl = document.querySelector("#temp");
+    const humidityEl = document.querySelector("#humidity");
+    const wind = document.querySelector("#wind");
+    
 
+
+    const city = cityName.value;
+    // TODO use a different api to get the longitude and latitude
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${appId}`;
 
     fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-            temp.textContent = data.main.temp;
-            humidity.value = data.main.humidity;
-            wind.value = data.wind.speed;
-            uvRequest(lat, lon);
+        .then(response => response.json())
+        .then(data => {
+            console.log (data);
+            // const { coord: { lat }, coord: { lon }, main: { temp }, main: { humidity }, wind: { speed } } = data;
+            // tempEl.innerHTML = temp;
+            // humidityEl.innerHTML = humidity;
+            // wind.innerHTML = speed;
+            // uvRequest(lat, lon);
         });
 }
 
-function uvRequest(lat, lon) {
-    var uvRequestUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + appId;
+uvRequest = (lat, lon) => {
+    const uvRequestUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units-imperial&exclude=minutely,hourly,alerts&appid=${appId}`;
+    // exclude=minutely,hourly,alerts&
     fetch(uvRequestUrl)
-        .then(function (Data) {
-            console.log("uvData:"+Data);
+        .then (blob => blob.json())
+        .then(data => {
+            const uv = document.querySelector("#uv");
+            console.log (data);
+            const {current :{uvi}} = data;
+            uv.innerHTML = uvi;
         });
 }
 citySearchEl.addEventListener('click', cityClickHandler);
